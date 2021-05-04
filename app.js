947f2f927cc5598a491f733c85780a1a28eeabcc
Bapat Nachiket <bapat@ssr.local>
2021-05-04 17:21:42 +0200

    lesson 1 :using the console

---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+console 
+*/
+console.log("hello");
+console.log(132);
+let greeting = "hello";
+console.log(greeting);
+console.log(true);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.error("error");
+console.clear();
+console.warn("achtung");
+console.time('hello');
+console.log("hello");
+console.log("hello");
+console.timeEnd('hello');
