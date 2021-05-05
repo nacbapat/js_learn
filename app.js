efd5edb26e1bb1f57418f61f5d56923c6e8f286f
Bapat Nachiket <bapat@ssr.local>
2021-05-05 14:26:55 +0200

    lesson 6: strings

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const num1  = 100;
-const num2  = 50;
+const firstName = 'Andreas';
+const lastName = 'Gotter';
 
-let val;
-val = num1  + num2;
+const stringer = 'hello there my name is lorry';
+const tags  = 'web design, web dev';
 
-//Math
-val = Math.E;
-val = Math.round(Math.PI);
-val = Math.floor(2.8);
-val = Math.abs(-3);
-val = Math.min(2,3,4,-6);
-val = Math.max(2,3,4,-6);
+let val ;
+val = 'his name is ' + firstName + ' '+ lastName;
+
+//escaping very imp
+val = 'that\'s awesome i can\'t wait' ;
+
+val = firstName.concat(' ',lastName);
+
+//change case
+val = firstName.toUpperCase();
+val = firstName.indexOf('s');
+val = firstName.lastIndexOf('s');   //if not there then  -1 is returned
+val = firstName.charAt('2');
+val = firstName.charAt(firstName.length -1);
+val = firstName.substring(0,3);   //same as slice, slice is used with arrays
+val = firstName.slice(-3);
+
+val = stringer.split(' ');    //brilliant to do data input changes
+val = stringer.replace('hello', 'bye');
+val = stringer.includes('hola');
 console.log(val);
