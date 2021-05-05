af0c1a80f8a8373eae42e99189052b519ed94a61
Bapat Nachiket <bapat@ssr.local>
2021-05-05 17:55:51 +0200

    undefined usage in JS

--- app.js
+++ app.js
@@ -1,41 +1,11 @@
-//object literals
+//if else
 
-const person = {
-  firstName : 'Andreas',
-  lastName : 'Gotter',
-  age: 44,
-  email: 'hello@gmail.com',
-  hobbies : ['music', 'programming'],
-  address : {
-    area: 'brand',
-    state : 'NRW',
-  },
-  getBirthyear : function () {
-    return 2021 - this.age; 
-  }
+const id = 100;
+
+if(typeof id !== 'undefined'){      //check if it there or not at all
+  console.log(`the id is ${id}`);
 }
-
-
-
-let val ;
-
-
-val = person;
-
-val =person.firstName; 
-val = person['lastName'];
-val = person.hobbies[0];
-val = person.address.area;
-val = person.getBirthyear();
-
-const people = [
-  {name : 'lisa', age : 24},
-  {name : 'lars', age : 25},
-  {name : 'chris', age : 45},
-];
-
-for (let index = 0; index < people.length; index++) {
-  console.log(people[index].name);
+else
+{
+console.log(`id existential crisis `);
 }
-
-console.log(val);
