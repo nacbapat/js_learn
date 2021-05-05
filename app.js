8244e0e2940316762b5eb091448ae82f00916ebb
Bapat Nachiket <bapat@ssr.local>
2021-05-05 14:04:50 +0200

    lesson 4: type conversion and coersion

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-//primitive
+// type conversion
 
-const namer = 'John Doe';
 
-const age = 45;
+let val;
 
-const haskids  = true;
-//null
-const car = null;
-//undef
-let test;
-//symbol
-const sym = Symbol();
+val = 4+4;
 
-//array
+//date to string
 
-const hobbies  = ['movies','music'];
+val = String(new Date());
 
-const address  = {
-  city: 'boston',
-  state :'NY'
-}
-const today = new Date();
-console.log (today);
 
-console.log (typeof today);
+//number to string
+
+val = (5).toString();
+
+// string to number
+
+val = Number([1,2,3]);
+
+val = parseFloat('100.88');
+
+
+
+console.log(val);
+console.log(typeof val);
+valstring  = String(val);
+console.log(val.toFixed(3));// only wokrs on numbers
+
+const  val1  = '5';
+const val2  = 6;
+const sum = Number(val1  + val2);   //implicit automatic conversion !!!! SO COOL !!!  type coersion !!
+
+console.log(typeof sum);
+console.log(sum)
