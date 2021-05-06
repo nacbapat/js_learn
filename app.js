9dd874ec628038388a4c4e4a84e2972e72ff7a04
Bapat Nachiket <bapat@ssr.local>
2021-05-06 16:48:47 +0200

    lesson 10: functions

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-//if else
+//function declaratoin
 
-const id = 100;
 
-if(typeof id !== 'undefined'){      //check if it there or not at all
-  console.log(`the id is ${id}`);
+function greet(firstName = 'John', lastName = ' Doe') { //function defaults here
+  return 'hello ' + firstName + lastName;
 }
-else
-{
-console.log(`id existential crisis `);
+
+console.log(greet('John',' asdfads'));
+console.log(greet());
+
+
+//function exporessions 
+//here the function is anaoónymous but the function is assigned to the variable and one can then access the variable as if it were a function. 
+
+const increment  = function (_initial =0) {
+
+  return _initial + 1;
+  
 }
+
+console.log(increment(44));
+
+//Immediately invokable functions IIFE or inline ones 
+//these must end with a paranthesis
+(function (name) {
+  console.log("hello " + name);
+})('Brad');
+
+
+//property functions (methods of objects)
+
+const divya ={    //const only declares that divya will be an object
+
+  cry : function () {
+    console.log("whaaa teamviewer is not working");
+  }
+}
+
+divya.cry();
