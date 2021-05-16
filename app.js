dafef03c9ed7c0d634f00850d4d98b84b32aa874
nacbapat <nacbapat@gmail.com>
2021-05-16 22:24:33 +0200

    DOM create elements

--- app.js
+++ app.js
@@ -1,51 +1,20 @@
-let val ;
-
-const list = document.querySelector('ul.collection');
-var listitem = document.querySelector('li.collection-item');  //will select first one 
-var listitem = document.querySelector('li.collection-item:first-child');  
-
-val = list;
-
-//get child nodes
-val = list.childNodes;  //this will return node list
-
-//get children    //more used !!
-val = list.children;    //this will return html collection
-
-// 1 element
-//2 - atribute
-// 3 - text node      //this is the line bread
-// 8 -comment
-//9 - document itself
-// 10 -doctype
-
-val = list.childNodes[0].nodeName;
-val = list.childNodes[1].nodeType;
-
-val = list.children[1].textContent = 'changed text';
-//children of children
-list.children[3].children[0].id = 'new-id';
-val = list.children[3].children[0];
-
-//first
-val = list.firstChild;    //text node
-val = list.firstElementChild; //actual element //more used
-
-//last
-val = list.lastChild;    //text node
-val = list.lastElementChild; //actual element //more used
-
-//count the child elements
-val =list.childElementCount;
-
-//get parent node
-val = listitem.parentElement;
-val =listitem.parentElement.parentElement
-//sibing
-val =listitem.nextSibling;    //this will catch line breaks
-val =listitem.nextElementSibling; //this will eliminate the line breaks
+//create element
+const li = document.createElement('li');
+li.className = 'collection-item';
+li.id = 'new-item'
+li.setAttribute('title','new Item');
 
 
-// console.log(listitem);
-console.log(val);
+//create text node
+li.appendChild(document.createTextNode('hello world')); //creates between <li> </li>
 
+const link = document.createElement('a');
+link.className = "delete-item secondary-content";
+link.innerHTML = '<i class="fa fa-remove"></i>';
+
+//append link into li
+li.appendChild(link);
+
+//append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+console.log(li);
