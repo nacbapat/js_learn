da8d00d87c89771f4c303620ed84673ec8b42b7d
Nachiket Bapat <nacbapat@gmail.com>
2022-03-24 00:51:14 +0100

    mouse events

--- app.js
+++ app.js
@@ -1,45 +1,23 @@
-const clearTasks1 = document.getElementsByClassName('clear-tasks');
-//or
-// const clearTasks2 = document.querySelector('.clear-tasks'); //when using queryselector pass the class name like in CSS
+const clearBtn = document.querySelector('.clear-tasks');
+const card = document.querySelector('.card');
+const heading = document.querySelector('h5');
 
-// console.log(clearTasks2);
+clearBtn.addEventListener('click',runEvent);
+clearBtn.addEventListener('dblclick',runEvent);
+clearBtn.addEventListener('mousedown',runEvent);
+clearBtn.addEventListener('mouseup',runEvent);
+clearBtn.addEventListener('mouseenter',runEvent);
+clearBtn.addEventListener('mouseleave',runEvent);
+clearBtn.addEventListener('mouseout',runEvent); //if there are any elements inside the button then this will be treiggered with leaving that element
+card.addEventListener('mouseout',runEvent); //if there are any elements inside the button then this will be treiggered with leaving that element
 
-// const clearTasks3 = document.querySelector('.clear-tasks').addEventListener('click',function (e) {
-//   console.log('hello world'); 
-//   //e.preventDefault(); same as using href="#"
-// }); 
 
 
-const clearTasks3 = document.querySelector('.clear-tasks').addEventListener('click',onClick); 
 
-function onClick(e) {
-  //console.log('clicked');
+//event handler
 
-  let val;
-
-  val = e;
-  val = e.target;
-  val = e.target.id;
-  val = e.target.className;
-  val = e.target.classList;
-  e.target.innerText = 'HEllo';
-  console.log(val)
-
-  //event type
-
-  val = e.type;
-  console.log(val)
-
-  //timestamp
-  val = e.timestamp;
-  val = e.clientY;
-  val = e.clientX;
-  val = e.offsetY;    //relative to button
-  val = e.offsetX;
-  
-  console.log(val)
-
-  
-
-  
+function runEvent(e) {
+  console.log(`Event Type: ${e.type}`);
+  heading.innerHTML = `mouseX: ${e.offsetX} mousey: ${e.offsetY}`;
+  document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},40)`;
 }
