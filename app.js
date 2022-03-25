822b6e0d3d1166188fae9a6ba4dcd2cc7fdc6e08
Nachiket Bapat <nacbapat@gmail.com>
2022-03-25 14:24:14 +0100

    event delegation

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 //event bubbling
 
-document.querySelector('.card-title').addEventListener('click',function () {
-  console.log('card title');
-})
+// document.querySelector('.card-title').addEventListener('click',function () {
+//   console.log('card title');
+// })
 
-document.querySelector('.card-content').addEventListener('click',function () {
-  console.log('card content');
-})
+// document.querySelector('.card-content').addEventListener('click',function () {
+//   console.log('card content');
+// })
 
-document.querySelector('.card').addEventListener('click',function () {
-  console.log('card');
-})
+// document.querySelector('.card').addEventListener('click',function () {
+//   console.log('card');
+// })
+
+//event delegation
+
+document.body.addEventListener('click',deleteItem);
+
+function deleteItem(e) {
+  if(e.target.className === 'delete-item secondary-content'){
+    console.log('delete item');
+  }
+  if(e.target.parentElement.classList.contains('delete-item')){
+    console.log('work');
+    e.target.parentElement.parentElement.remove();
+  }
+}
