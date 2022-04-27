6ccef37916066cafe982915f5e099e6b24cfe522
Nachiket Bapat <nacbapat@gmail.com>
2022-04-27 16:45:16 +0200

    JS Object create

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
-function Person(firstname,lastname) {
-    this.firstName = firstname;
+const personPrototypes = {
+  greeting: function() {
+    return `Hello there ${this.firstname} ${this.lastName}`;
+  },
+
+  getsMarried: function(lastname){
     this.lastName = lastname;
+  }
 }
 
-Person.prototype.greeting = function () {
-  return `Hello there ${this.firstName} ${this.lastName}`;
-}
 
-const person1  = new Person('holla','doner');
+const mary = Object.create(personPrototypes);
+mary.firstname = 'MAry';
+mary.lastName = 'asdf';
 
-console.log(person1.greeting());
+console.log(mary);
 
-function Customer(firstName,lastName,phone,membership){
-  Person.call(this,firstName,lastName); //call is base function to call the constructor of Person
-
-  this.phone = phone;
-  this.membership = membership;
-}
-
-//inherit person into customer !!
-Customer.prototype = Object.create(Person.prototype);
-//make Customer.prototype return Customer()
-Customer.prototype.constructor = Customer;
-
-//Create Customer
-
-const customer1 = new Customer("tom",'smith','38833-44534-33','std');
-
-
-Customer.prototype.greeting = function () {
-  return `Hello there ${this.firstName} ${this.lastName} membership id: ${this.membership}`;
-}
-
-console.log(customer1);
-console.log(customer1.greeting());
+const brad = Object.create(personPrototypes,{
+  firstname: {value: 'peter'},
+  lastName: {value: 'pfeiffer'},
+  age: {value:36}
+});
